3b2f6feae5afc138c21caca159f9344b88a656a1
Tomasz Bednarz <tombed6@gmail.com>
2025-09-08 21:25:05 +0200

    fix config for booksById

--- src/test/java/com/redundantcich/crudauto/config/Config.java
+++ src/test/java/com/redundantcich/crudauto/config/Config.java
@@ -3,28 +3,28 @@ package com.redundantcich.crudauto.config;
 import net.thucydides.model.environment.SystemEnvironmentVariables;
 import net.thucydides.model.util.EnvironmentVariables;
 
-    public class Config {
+public class Config {
 
-        private static final EnvironmentVariables vars = SystemEnvironmentVariables.createEnvironmentVariables();
-        private static final String env = System.getProperty("environment", "dev");
+    private static final EnvironmentVariables vars = SystemEnvironmentVariables.createEnvironmentVariables();
+    private static final String env = System.getProperty("environment", "dev");
 
-        public static String getBaseUrl() {
-            return vars.getProperty("environments." + env + ".base.url");
-        }
-
-        public static String getBooksEndpoint() {
-            return vars.getProperty("environments." + env + ".endpoints.books");
-        }
-
-        public static String getBookByIdEndpoint(String id) {
-            return vars.getProperty("environments." + env + ".endpoints.books").replace("{id}", id);
-        }
-
-        public static String getApiUser() {
-            return System.getenv("API_USER");
-        }
-
-        public static String getApiPass() {
-            return System.getenv("API_PASS");
-        }
+    public static String getBaseUrl() {
+        return vars.getProperty("environments." + env + ".base.url");
     }
+
+    public static String getBooksEndpoint() {
+        return vars.getProperty("environments." + env + ".endpoints.books");
+    }
+
+    public static String getBookByIdEndpoint(String id) {
+        return vars.getProperty("environments." + env + ".endpoints.bookById").replace("{id}", id);
+    }
+
+    public static String getApiUser() {
+        return System.getenv("API_USER");
+    }
+
+    public static String getApiPass() {
+        return System.getenv("API_PASS");
+    }
+}
