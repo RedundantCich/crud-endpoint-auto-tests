10b99304802f229087f51451f5a6f05e4411db5c
Tomasz Bednarz <tombed6@gmail.com>
2025-09-11 20:46:46 +0200

    auth steps implementation

--- src/test/java/com/redundantcich/crudauto/steps/AuthenticationSteps.java
+++ src/test/java/com/redundantcich/crudauto/steps/AuthenticationSteps.java
@@ -1,11 +1,17 @@
 package com.redundantcich.crudauto.steps;
 
 import io.cucumber.java.en.Given;
+import net.serenitybdd.core.Serenity;
 
 public class AuthenticationSteps {
 
     @Given("I am an authenticated API user")
     public void iAmAnAuthenticatedApiUser() {
-        // For now, empty since Config already contains credentials.
+        Serenity.setSessionVariable("auth").to(true);
+    }
+
+    @Given("I don't have valid authentication credentials")
+    public void iDonTHaveValidAuthenticationCredentials() {
+        Serenity.setSessionVariable("auth").to(false);
     }
 }
